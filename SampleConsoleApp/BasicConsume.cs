6f520dfbca79fef4e270b5363483eb20b647076a
Ertuğrul Demirkaya <ertugrulldemirkaya@gmail.com>
2020-09-28 04:07:03 +0300

    commit
    commit

---
+++ SampleConsoleApp/BasicConsume.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleConsoleApp
+{
+    public static class BasicConsume
+    {
+        public static void DoConsume()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "HR",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.Span;
+                    var message = Encoding.UTF8.GetString(body);
+                    Person person = JsonConvert.DeserializeObject<Person>(message);
+                    Console.WriteLine($" Mr.  {person.Name} {person.Surname} - {person.Department}");
+                };
+                channel.BasicConsume(queue: "HR", 
+                    noLocal: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" You are hired !!! ");
+            }
+        }
+    }
+}
+internal class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Department { get; set; }
+}
---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                Department = "IT",
+                Name = "John",
+                Surname = "Doe"
+            };
+
+            var connFact = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = connFact.CreateConnection())
+            using (IModel model = connection.CreateModel())
+            {
+                model.QueueDeclare(queue: "HR",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                string message = JsonConvert.SerializeObject(person);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                model.BasicPublish(exchange: "",
+                       routingKey: "HR",
+                       basicProperties: null,
+                       body: body);
+
+
+                Console.WriteLine($"Person Info: {person.Name}  {person.Surname} - {person.Department}");
+
+                BasicConsume.DoConsume();
+            }
+
+
+
+
+        }
+    }
+
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+    }
+}
